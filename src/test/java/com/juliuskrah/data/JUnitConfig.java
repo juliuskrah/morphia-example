f30798e4c4537072201e574f02bff1375762bbc0
Julius Krah <juliuskrah@gmail.com>
2017-05-20 21:18:35 +0000

    Removed 'Main' method in Test configuration

--- src/test/java/com/juliuskrah/data/JUnitConfig.java
+++ src/test/java/com/juliuskrah/data/JUnitConfig.java
@@ -1,13 +1,10 @@
 package com.juliuskrah.data;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
 
 @Profile("test")
 @SpringBootApplication
 public class JUnitConfig {
-	public static void main(String[] args) {
-		SpringApplication.run(JUnitConfig.class, args);
-	}
+	
 }
